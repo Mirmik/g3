ab90ec783aa6deaf2d8846330571bbd7fdb559d9
mirmik <mirmikns@yandex.ru>
2018-07-23 19:31:47 +0300

    a

--- g3/brocker.h
+++ g3/brocker.h
@@ -1,14 +1,67 @@
 #ifndef G3_BROCKER_H
 #define G3_BROCKER_H
 
-namespace g3 {
-	struct subscriber {
+#include <g1/tower.h>
+#include <unordered_map>
 
+namespace g3 {
+	struct theme;
+
+	enum class frame_type {
+		SUBSCRIBE = 0,
+		PUBLISH = 1,
+		MESSAGE = 2,
+	};
+
+	struct subheader {
+		frame_type type;
+
+		union {
+			struct {
+				uint8_t thm_size;
+				uint16_t dat_size;
+			};
+
+			struct {
+				uint8_t thm_size;
+				QoS::
+			};
+		};
+	};
+
+	struct subscribe {
+		theme* 		tptr;
+
+		union {
+			struct { ///< g3 
+				g1::QoS qos;
+			};
+
+			struct { ///< g2 socket
+								
+			};
+		};
+	};
+
+	struct subscriber {
+		std::unordered_map<std::string, subscribe> themes;	
+
+		uint8_t* 	raddr_ptr;
+		size_t 		raddr_len;
+	
+		uint8_t 	proto;
+
+		
 	};
 
 	struct theme {
-
+		std::unordered_map<std::string, subscriber*> subscribers;
 	};
+
+	void incoming(g1::packet* pack);
+	void incoming_dataframe(const char* data, size_t size);
+
+
 }
 
 #endif
